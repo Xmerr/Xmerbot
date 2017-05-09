3aa1f58a152500af8bc04933e4d593d9e384952a
root <root@li933-55.members.linode.com>
2017-05-09 14:31:08 +0000

    added index to blank file extensions

--- appFiles/httpServer.js
+++ appFiles/httpServer.js
@@ -1,7 +1,9 @@
+var path = require('path');
+
 module.export = (() => {
     var express = require('express'),
         app = express();
 
-    app.use(express.static(__dirname + '../files'));
+    app.use(express.static(path.join(__dirname + '/../files'),{index:false,extensions:['html']}));
     app.listen(process.env.port);
 })();
