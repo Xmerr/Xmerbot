fdef011cee0ff6052bc3894d25a268b1e9aade52
Wiley Hilton <wileyhilton@gmail.com>
2017-05-09 00:47:06 +0000

    fixed gitignore

---
+++ actions/load.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (output, user, data, newCall) => {
+    if(!data.user) {
+        data.user = user;
+    }    
+    
+    var dir = path.resolve(__dirname, "../files/" + data.user);
+    
+    if (!fs.existsSync(dir)){
+        output(`${data.user} doesn't have any files`);
+        return;
+    }
+    
+    
+    fs.readdir(dir, (err, items) => {
+        if(err){
+            console.log(err);
+        }
+        
+        for(var i = items.length - 1; i >= 0; i--) {
+            if(items[i].split('.')[0] === data.fileName) {
+                if(global.disc) {
+                    global.disc.uploadFile({
+                        to: data.channelID,
+                        file: dir + "/" + items[i],
+                        message: "Here you go"
+                    });
+                    return;
+                }
+                
+                output('Document found, but I\'m not connected to a discord channel');
+            }
+        }
+        
+        output(`${data.user} doesn't have any files named ${data.fileName}`);
+    });
+};
