5e57a49a02a6e6165b4577c2e7271ef2f6296a62
Wiley Hilton <wileyhilton@gmail.com>
2017-05-09 13:27:24 +0000

    fixed issue serving static files

--- appFiles/httpServer.js
+++ appFiles/httpServer.js
@@ -2,6 +2,6 @@ module.export = (() => {
     var express = require('express'),
         app = express();
 
-    app.use(express.static(__dirname + '../files'));
+    app.use(express.static(__dirname + '/../files'));
     app.listen(process.env.port);
 })();
