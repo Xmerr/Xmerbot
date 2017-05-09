1d63124600bc0ab2b6313b8c789770941fae66c1
Wiley Hilton <wileyhilton@gmail.com>
2017-05-09 14:32:57 +0000

    fixed the terrible merger i did over ssh

--- appFiles/httpServer.js
+++ appFiles/httpServer.js
@@ -4,7 +4,9 @@ module.export = (() => {
     var express = require('express'),
         app = express();
 
-    app.use(express.static(path.join(__dirname + '/../files'),{index:false,extensions:['html']}));
->>>>>>> 2d2affc494e18b9f3fe2fe6d06cb2710914d04da
+    app.use(express.static(path.join(__dirname + '/../files'),{
+        index:false,
+        extensions:['html']
+    }));
     app.listen(process.env.port);
 })();
