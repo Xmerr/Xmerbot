de96daf66cf411ad8a49f6e1dc058d3b824fac81
Wiley Hilton <wileyhilton@gmail.com>
2017-05-15 20:25:00 +0000

    Added a homepage

--- site/httpServer.js
+++ site/httpServer.js
@@ -8,5 +8,11 @@ module.export = (() => {
         index: false,
         extensions: ['webp', 'html']
     }));
+    app.use('/public', express.static(path.join(__dirname + './publicFiles')));
+    
+    app.get('/', (req, res) => {
+       res.send("This is going to be Xmer's portfolio site. Which is going to be super awesome... Once it's done"); 
+    });
+    
     app.listen(process.env.port);
 })();
